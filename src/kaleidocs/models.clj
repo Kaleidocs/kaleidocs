3cab3202337334bffb4662dcf46f5d5a8ca69de7
Hoàng Minh Thắng <p@banphim.net>
2014-04-16 23:24:54 +0700

    remove unnecessary transform functions

--- src/kaleidocs/models.clj
+++ src/kaleidocs/models.clj
@@ -31,22 +31,6 @@
                :docgroup_id :profile_id]})
 
 (defdb clogdb db-spec)
-(defn transform-data
-  "Get v by query SQL. Transform it before sending out"
-  [{data :data :as v}]
-  (let [v (dissoc v :data)]
-    (if (seq data)
-      (merge v (parse-string data true))
-      v)))
-
-(defn prepare-data [& skipped-fields]
-  (fn [{data :data :as v}]
-    (let [data
-          (apply dissoc v
-                 skipped-fields)
-          v (apply dissoc v (keys data))]
-      (assoc v :data (generate-string data)))))
-
 
 (defentity document)
 (defentity docgroup)
